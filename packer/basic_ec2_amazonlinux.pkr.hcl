34e4ac0304c0c866eb6d985e647bf1c9bcc374a5
glennbech <glenn.bech@gmail.com>
2021-08-30 23:19:33 +0200

    wip

--- packer/basic_ec2_amazonlinux.pkr.hcl
+++ packer/basic_ec2_amazonlinux.pkr.hcl
@@ -1,9 +1,10 @@
 source "amazon-ebs" "basic-example" {
   region =  "eu-north-1"
-  source_ami =  "ami-0d441f5643da997cb"
+  source_ami =  "ami-0fce25857d9f86070"
   instance_type =  "t3.micro"
   ami_name =  "seat_template"
   ssh_username = "ec2-user"
+
 }
 
 build {
@@ -13,7 +14,7 @@ build {
 
   provisioner "file" {
     source = "packer/boto3_example.py"
-    destination = "boto3_example.py"
+    destination = "/home/ssm-user/boto3_example.py"
   }
 }
 
--- packer/boto3_example.py
+++ packer/boto3_example.py
@@ -1,19 +1,9 @@
 import boto3
-from botocore.exceptions import ClientError
-
-with open("hello.txt", "w") as f:
-    f.write('Hello World!')
-
-s3Client = boto3.client('s3')
-
-# upload a file
-try:
-    response = s3Client.upload_file(
-        Filename = 'hello.txt',
-        Bucket = 'my-test-bucket',
-        Key = 'hello.txt',
-        ExtraArgs={'ACL': 'public-read'}
-    )
-    print(response)
-except ClientError as e:
-    print(e)
+ec2client = boto3.client('ec2')
+response = ec2client.describe_instances()
+for reservation in response["Reservations"]:
+    for instance in reservation["Instances"]:
+        # This sample print will output entire Dictionary object
+        print(instance)
+        # This will print will output the value of the Dictionary key 'InstanceId'
+        print(instance["InstanceId"])
